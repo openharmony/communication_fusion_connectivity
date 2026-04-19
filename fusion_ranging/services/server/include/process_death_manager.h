849bc9b46a97a5ad8dad652b86d1e0c41743df91
haoliang2026 <haoliang1@huawei.com>
2026-04-19 20:31:15 +0800

    update: 更新文件 process_death_manager.h 更新death manager
    Signed-off-by: haoliang2026 <haoliang1@huawei.com>

--- fusion_ranging/services/server/include/process_death_manager.h
+++ fusion_ranging/services/server/include/process_death_manager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -7,18 +7,76 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributd under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef FUSION_RANGING_PROCESS_DEATH_MANAGER_H
 #define FUSION_RANGING_PROCESS_DEATH_MANAGER_H
+
+#include <functional>
+#include <map>
+#include <memory>
+#include <mutex>
+
+#include "iremote_object.h"
+#include "refbase.h"
 
 namespace OHOS {
 namespace FusionRanging {
 
-} // namespace FusionRanging
-} // namespace OHOS
+class ProcessDeathManager {
+public:
+    using DeathCallback = std::function<void(int32_t uid)>;
 
-#endif // FUSION_RANGING_PROCESS_DEATH_MANAGER_H
+    ProcessDeathManager() = default;
+    ~ProcessDeathManager() = default;
+
+    void SetDeathCallback(DeathCallback callback);
+    bool RegisterProcessDeath(int32_t uid, const sptr<IRemoteObject> &remoteObject);
+    bool UnregisterProcessDeath(int32_t uid);
+    bool HasProcessDeathHandler(int32_t uid) const;
+    void ClearAll();
+
+private:
+    class ProcessDeathRecipient : public IRemoteObject::DeathRecipient {
+    public:
+        explicit ProcessDeathRecipient(int32_t uid, DeathCallback callback) : uid_(uid), callback_(callback) {}
+
+        void OnRemoteDied(const wptr<IRemoteObject> &) override
+        {
+            if (callback_) {
+                callback_(uid_);
+            }
+        }
+
+        ~ProcessDeathRecipient() override = default;
+
+    private:
+        int32_t uid_;
+        DeathCallback callback_;
+    };
+
+    struct ProcessDeathHandler {
+        sptr<IRemoteObject::DeathRecipient> recipient;
+        wptr<IRemoteObject> remoteObject;
+
+        ProcessDeathHandler() = default;
+        ProcessDeathHandler(sptr<IRemoteObject::DeathRecipient> r, const wptr<IRemoteObject> &obj)
+            : recipient(r),
+              remoteObject(obj)
+        {
+        }
+    };
+
+    mutable std::mutex mutex_;
+    std::map<int32_t, ProcessDeathHandler> deathHandlers_;
+    DeathCallback deathCallback_;
+};
+
+}  // namespace FusionRanging
+}  // namespace OHOS
+
+#endif  // FUSION_RANGING_PROCESS_DEATH_MANAGER_H
