0bac340489478d5bd7f8831d51ca72a46468147b
haoliang2026 <haoliang1@huawei.com>
2026-04-19 19:48:01 +0800

    update: 更新文件 napi_fusion_ranging_constant.cpp 同步napi
    Signed-off-by: haoliang2026 <haoliang1@huawei.com>

--- fusion_ranging/frameworks/napi/src/napi_fusion_ranging_constant.cpp
+++ fusion_ranging/frameworks/napi/src/napi_fusion_ranging_constant.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Copyright (C) 2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -7,14 +7,122 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributd under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
+#include "napi_fusion_ranging_constant.h"
+#include "napi_parser_utils.h"
+
 namespace OHOS {
 namespace FusionRanging {
+namespace {
+constexpr int32_t RANGING_TYPES_NEARLINK_HADM = 1;
 
-} // namespace FusionRanging
-} // namespace OHOS
+constexpr int32_t RANGING_STATE_STOPPED = 0;
+constexpr int32_t RANGING_STATE_STARTING = 1;
+constexpr int32_t RANGING_STATE_STARTED = 2;
+
+constexpr int32_t RANGING_ROLE_INITIATOR = 0;
+constexpr int32_t RANGING_ROLE_RESPONDER = 1;
+
+constexpr int32_t RANGING_STOPPED_CAUSE_NO_ERROR = 0;
+constexpr int32_t RANGING_STOPPED_CAUSE_INTERNAL_ERROR = 1;
+constexpr int32_t RANGING_STOPPED_CAUSE_BUSINESS_CONFLICT = 2;
+constexpr int32_t RANGING_STOPPED_CAUSE_LIMITED_RESOURCE = 3;
+
+constexpr int32_t RANGING_CONFIDENCE_HIGH = 0;
+constexpr int32_t RANGING_CONFIDENCE_MEDIUM = 1;
+constexpr int32_t RANGING_CONFIDENCE_LOW = 2;
+}  // namespace
+
+napi_value NapiFusionRangingConstant::DefineJSConstant(napi_env env, napi_value exports)
+{
+    HILOGD("enter");
+    ConstantPropertyValueInit(env, exports);
+    napi_property_descriptor desc[] = {};
+    napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc);
+    HILOGI("end");
+    return exports;
+}
+
+napi_value NapiFusionRangingConstant::ConstantPropertyValueInit(napi_env env, napi_value exports)
+{
+    HILOGD("enter");
+    napi_value rangingTypesObj = RangingTypesInit(env);
+    napi_value rangingStateObj = RangingStateInit(env);
+    napi_value rangingRoleObj = RangingRoleInit(env);
+    napi_value rangingStoppedCauseObj = RangingStoppedCauseInit(env);
+    napi_value rangingConfidenceObj = RangingConfidenceInit(env);
+    napi_property_descriptor exportFuncs[] = {
+        DECLARE_NAPI_PROPERTY("RangingTypes", rangingTypesObj),
+        DECLARE_NAPI_PROPERTY("RangingState", rangingStateObj),
+        DECLARE_NAPI_PROPERTY("RangingRole", rangingRoleObj),
+        DECLARE_NAPI_PROPERTY("RangingStoppedCause", rangingStoppedCauseObj),
+        DECLARE_NAPI_PROPERTY("RangingConfidence", rangingConfidenceObj),
+    };
+    napi_define_properties(env, exports, sizeof(exportFuncs) / sizeof(*exportFuncs), exportFuncs);
+    HILOGI("end");
+    return exports;
+}
+
+napi_value NapiFusionRangingConstant::RangingTypesInit(napi_env env)
+{
+    HILOGD("enter");
+    napi_value rangingTypes = nullptr;
+    napi_create_object(env, &rangingTypes);
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingTypes, RANGING_TYPES_NEARLINK_HADM, "NEARLINK_HADM");
+    return rangingTypes;
+}
+
+napi_value NapiFusionRangingConstant::RangingStateInit(napi_env env)
+{
+    HILOGD("enter");
+    napi_value rangingState = nullptr;
+    napi_create_object(env, &rangingState);
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingState, RANGING_STATE_STOPPED, "STATE_STOPPED");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingState, RANGING_STATE_STARTING, "STATE_STARTING");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingState, RANGING_STATE_STARTED, "STATE_STARTED");
+    return rangingState;
+}
+
+napi_value NapiFusionRangingConstant::RangingRoleInit(napi_env env)
+{
+    HILOGD("enter");
+    napi_value rangingRole = nullptr;
+    napi_create_object(env, &rangingRole);
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingRole, RANGING_ROLE_INITIATOR, "ROLE_INITIATOR");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingRole, RANGING_ROLE_RESPONDER, "ROLE_RESPONDER");
+    return rangingRole;
+}
+
+napi_value NapiFusionRangingConstant::RangingStoppedCauseInit(napi_env env)
+{
+    HILOGD("enter");
+    napi_value rangingStoppedCause = nullptr;
+    napi_create_object(env, &rangingStoppedCause);
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingStoppedCause, RANGING_STOPPED_CAUSE_NO_ERROR, "NO_ERROR");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingStoppedCause, RANGING_STOPPED_CAUSE_INTERNAL_ERROR,
+                                                  "INTERNAL_ERROR");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingStoppedCause, RANGING_STOPPED_CAUSE_BUSINESS_CONFLICT,
+                                                  "BUSINESS_CONFLICT");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingStoppedCause, RANGING_STOPPED_CAUSE_LIMITED_RESOURCE,
+                                                  "LIMITED_RESOURCE");
+    return rangingStoppedCause;
+}
+
+napi_value NapiFusionRangingConstant::RangingConfidenceInit(napi_env env)
+{
+    HILOGD("enter");
+    napi_value rangingConfidence = nullptr;
+    napi_create_object(env, &rangingConfidence);
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingConfidence, RANGING_CONFIDENCE_HIGH, "HIGH");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingConfidence, RANGING_CONFIDENCE_MEDIUM, "MEDIUM");
+    FusionConnectivity::SetNamedPropertyByInteger(env, rangingConfidence, RANGING_CONFIDENCE_LOW, "LOW");
+    return rangingConfidence;
+}
+
+}  // namespace FusionRanging
+}  // namespace OHOS
